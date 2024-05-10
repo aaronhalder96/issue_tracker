9b5d0326aa6c74cfd710b87d63f91d20bfe2b686
aaronhalder-pcube <aaron@pcubedigital.com>
2024-05-10 18:52:07 +0600

    Added simplemde

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import NavBar from "./NavBar";
 import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+import NavBar from "./NavBar";
 import { Theme } from "@radix-ui/themes";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,9 +22,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Theme>
+    <html lang="en" className={inter.variable}>
+      <body>
+        <Theme accentColor="violet" radius="large">
           <NavBar />
           <main className="p-5">{children}</main>
         </Theme>
